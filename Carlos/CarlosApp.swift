749143a3c95609bff31901681d4278a2a3a97cd0
Zang Zhihao <zhihao.zang@linecorp.com>
2021-05-16 17:55:52 +0900

    Initial Commit

---
+++ Carlos/CarlosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarlosApp.swift
+//  Carlos
+//
+//  Created by Zang Zhihao on 2021-05-16.
+//
+
+import SwiftUI
+
+@main
+struct CarlosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
